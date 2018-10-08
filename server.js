adc40cccff3d670a4c5b596c5931600f70a4f116
Deepesh316 <deepeshkumarr316@gmail.com>
2018-10-08 10:27:50 +0530

    Setup Start Script and Heroku Port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.set('view engine', 'hbs');
@@ -15,6 +17,6 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {heading: 'Aboutt Page'});
 });
 
-app.listen(3000, () => {
-    console.log('Server Started');
+app.listen(port, () => {
+    console.log(`Server Started ${port}`);
 });
